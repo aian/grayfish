0d0b6c464650decf2615452f073c0d430aaf46d0
aian <aian@qune.jp>
2020-02-17 13:04:15 +0900

    Add unit testing module
    Belatedly we add the testing skelton ... :D

---
+++ test/local.h
@@ -0,0 +1,26 @@
+/*-
+ * This file is part of Grayfish project. For license details, see the file
+ * 'LICENSE.md' in this package.
+ */
+/*!
+** @file test/local.h
+** @brief Common definitions.
+*/
+#ifndef TEST_LIBGF_LOCAL_H_
+#define TEST_LIBGF_LOCAL_H_
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* TEST_LIBGF_LOCAL_H_ */
---
+++ test/main.c
@@ -0,0 +1,57 @@
+/*-
+ * This file is part of Grayfish project. For license details, see the file
+ * 'LICENSE.md' in this package.
+ */
+/*!
+** @file test/main.c
+** @brief The entry point for unit testing.
+*/
+#include <stdlib.h>
+
+#include <CUnit/Cunit.h>
+#include <CUnit/Console.h>
+
+#include "local.h"
+
+void
+gft_init(void) {
+  CU_initialize_registry();
+}
+
+void
+gft_prepare(void) {
+  
+}
+
+void
+gft_run(void) {
+  CU_console_run_tests();
+}
+
+void
+gft_cleanup(void) {
+  CU_cleanup_registry();
+}
+
+
+/*!
+** @brief The entry point
+**
+** @param argc [in] Count of the command argument.
+** @param argv [in] The vector of the command argument.
+*/
+
+int
+main(void) {
+  int ret = EXIT_SUCCESS;
+
+  gft_init();
+
+  gft_prepare();
+
+  gft_run();
+  
+  gft_cleanup();
+   
+  return ret;
+}
