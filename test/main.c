3c3b4371cc053336012840326696b5c788265b9d
aian <aian@qune.jp>
2020-02-17 15:36:06 +0900

    Add comments to test/main.c

--- test/main.c
+++ test/main.c
@@ -13,26 +13,47 @@
 
 #include "local.h"
 
+/*!
+** @brief Initialize the test module.
+*/
+
 void
 gft_init(void) {
   CU_initialize_registry();
 }
+
+/*!
+** @brief Prepare test suites
+**
+** Register tests. The concrete registering functions are declared in local.h.
+** And each tests are impremented in test-<module name>.c (e.g. The tests for
+** gf_path.c is impremented in test-path.c).
+*/
 
 void
 gft_prepare(void) {
   gft_path_add_tests();  // gf_path
 }
 
+/*!
+** @brief Run tests
+*/
+
 void
 gft_run(void) {
   CU_console_run_tests();
 }
+
+/*!
+** @brief Cleanup test module.
+*/
 
 void
 gft_cleanup(void) {
   CU_cleanup_registry();
 }
 
+/* -------------------------------------------------------------------------- */
 
 /*!
 ** @brief The entry point
